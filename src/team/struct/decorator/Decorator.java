f717f50573e13543a8eeb83658e550ae8880006d
housirzero <housirzero@gmail.com>
2015-09-01 16:25:38 +0800

    add decorator

---
+++ src/team/struct/decorator/Decorator.java
@@ -0,0 +1,21 @@
+package team.struct.decorator;
+
+/**
+ * 装饰者
+ *
+ */
+public abstract class Decorator implements Human {
+	private Human human;
+
+	public Decorator(Human human) {
+		this.human = human;
+	}
+
+	public void wearClothes() {
+		human.wearClothes();
+	}
+
+	public void walkToWhere() {
+		human.walkToWhere();
+	}
+}	
